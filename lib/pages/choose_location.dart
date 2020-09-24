301243ab5c513007880eb0ae9058d9c38168501a
drsuccess <a4surprise@gmail.com>
2020-09-24 15:18:13 +0100

    2nd - World Time

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,6 +6,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+
+    //simulate network request for a username
+    //await keywords runs the code first b4 another
+    //we can assign value to the builtFunction 'await'
+    //we use 'await' because each code depends on each other
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return('drSuccess');
+    });
+
+    //simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return ('Bio: vegan, musician & egg collector');
+    });
+
+    print('$username - $bio');
+  }
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    //Override and call the original function State<Choose..>
+    //iniState function runs once
+    super.initState();
+    getData();
+    print('Hey there!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +46,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose location screen'),
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
